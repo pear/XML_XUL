4eb75d8ad7e44f17e3c5891fed142616e56cb6c5
Stephan Schmidt <schst@php.net>
2004-06-26 08:55:24 +0000

    updated package file to reflect recent changes
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_XUL/trunk@162104 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ package.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * script to automate the generation of the
+ * package.xml file.
+ *
+ * $Id$
+ *
+ * @author      Stephan Schmidt <schst@php-tools.net>
+ * @package     XML_XUL
+ * @subpackage  Tools
+ */
+
+/**
+ * uses PackageFileManager
+ */ 
+require_once 'PEAR/PackageFileManager.php';
+
+/**
+ * current version
+ */
+$version = '0.8.1';
+
+/**
+ * current state
+ */
+$state = 'alpha';
+
+/**
+ * release notes
+ */
+$notes = <<<EOT
+XML_XUL now works with PHP5,
+fixed bug #1255,
+improved cloneElement() to allow cloning of complete subtrees
+EOT;
+
+/**
+ * package description
+ */
+$description = <<<EOT
+The XML User Interface Language (XUL) is a markup language for describing user interfaces.
+With XUL you can create rich, sophisticated cross-platform web applications easily.
+XML_XUL provides a API similar to DOM to create XUL applications. There is a PHP object for each XUL element, and the more complex widgets like grids, trees and tabboxes can easily be created with these objects.
+EOT;
+
+$package = new PEAR_PackageFileManager();
+
+$result = $package->setOptions(array(
+    'package'           => 'XML_XUL',
+    'summary'           => 'Class to build Mozilla XUL applications.',
+    'description'       => $description,
+    'version'           => $version,
+    'state'             => $state,
+    'license'           => 'PHP License',
+    'filelistgenerator' => 'cvs',
+    'ignore'            => array('package.php', 'package.xml'),
+    'notes'             => $notes,
+    'simpleoutput'      => true,
+    'baseinstalldir'    => 'XML',
+    'packagedirectory'  => './',
+    'dir_roles'         => array('docs' => 'doc',
+                                 'examples' => 'doc',
+                                 'tests' => 'test',
+                                 )
+    ));
+
+if (PEAR::isError($result)) {
+    echo $result->getMessage();
+    die();
+}
+
+$package->addMaintainer('schst', 'lead', 'Stephan Schmidt', 'schst@php.net');
+
+$package->addDependency('PEAR', '', 'has', 'pkg', false);
+$package->addDependency('XML_Util', '0.5.2', 'ge', 'pkg', false);
+$package->addDependency('XML_Parser', '1.1.0', 'ge', 'pkg', false);
+$package->addDependency('php', '4.2.0', 'ge', 'php', false);
+
+if (isset($_GET['make']) || (isset($_SERVER['argv'][1]) && $_SERVER['argv'][1] == 'make')) {
+    $result = $package->writePackageFile();
+} else {
+    $result = $package->debugPackageFile();
+}
+
+if (PEAR::isError($result)) {
+    echo $result->getMessage();
+    die();
+}
+?>
